3e539cee0af1cb47e892cbef518592dfff0dbe2a
TatyanaKuleshova <tanuscha@bk.ru>
2020-12-25 11:30:10 +0300

    25 декабря (урок 21)

--- herbs/articles/views.py
+++ herbs/articles/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'articles/index.html')
+
+def properties(request):
+    return render(request, 'articles/properties.html')
+
+def catalog(request):
+    return render(request, 'articles/catalog.html')
+
+def literatura(request):
+    return render(request, 'articles/literatura.html')
+
+def article(request):
+    return render(request, 'articles/article.html')
+
+def registration(request):
+    return render(request, 'articles/registration.html')
